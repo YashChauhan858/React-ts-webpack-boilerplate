37992fdd5bc31e20fff0eee438827117ecd96432
Yash Chauhan <yashchauhan858@gmail.com>
2023-05-19 10:15:01 +0530

    react query cahanges to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { store } from './Store/store';
 import App from './App';
 import ErrorBoundary from './ErrorLogger/ErrorLogger';
