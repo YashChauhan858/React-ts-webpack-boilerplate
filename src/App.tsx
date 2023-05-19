f8647fa20c5911842f6d2873b3fb158462e22f54
Yash Chauhan <yashchauhan858@gmail.com>
2023-05-19 10:04:52 +0530

    styles folder added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import './aoo.css';
+import styles from '@Styles/_global.scss';
 
-const App = () => <h1>This is react app</h1>;
+const App = () => (
+  <div style={styles}>
+    <h1 className="h1Class">hasd</h1>
+  </div>
+);
 
 export default App;
